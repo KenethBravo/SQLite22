5b7c8b25e4fed646093795861052ca829114cbab
keneth <kenethriascobravo@gmail.com>
2017-05-24 13:59:09 -0500

    sql lite

--- app/src/main/java/com/jonmid/sqlite/MainActivity.java
+++ app/src/main/java/com/jonmid/sqlite/MainActivity.java
@@ -37,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         dataUser.open();
 
 
+
+
         /*create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
